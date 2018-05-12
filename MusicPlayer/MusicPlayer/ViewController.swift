1015c2fe92ed2e904abe761c444ddfccf638cacc
escape_anaemia <hsj3171@naver.com>
2018-05-12 22:25:29 +0900

    empty fire to start Music Player project

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 한상준 on 2018. 5. 12..
+//  Copyright © 2018년 한상준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
