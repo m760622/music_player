68c37aff991f705d6576e26cf78a5329d1939482
andrew <andrew.ssumu@gmail.com>
2018-05-12 22:28:20 +0900

    This commit is for test git

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -6,10 +6,16 @@
 //  Copyright © 2018년 한상준. All rights reserved.
 //
 
+
+/*
+ git commit test
+ */
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
